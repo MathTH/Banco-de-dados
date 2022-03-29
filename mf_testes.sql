bdd48ec1349044bfdd32acd48be42e007d964144
MathTH <mathdhferreira@gmail.com>
2022-03-28 22:59:22 -0300

    Primeiro Banco

---
+++ mf_testes.sql
@@ -0,0 +1,9 @@
+CREATE TABLE pessoas (
+    id INT NOT NULL  PRIMARY KEY AUTOINCREMENT,
+    nome VARCHAR(30) NOT NULL
+    nascimento DATE 
+)
+
+INSERT INTO pessoas (nome, nascimento) VALUES ('Matheus Ferreira', '2003 02 19')
+INSERT INTO pessoas (nome, nascimento) VALUES ('Dhenifer Soares', '2003 05 07')
+INSERT INTO pessoas (nome, nascimento) VALUES ('Cleonir da Cunha', '1977 11 11')
